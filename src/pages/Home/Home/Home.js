4f64e23ddbb2509b0c890d01435ce706d7324336
Anikcse19 <anikdebcse@gmail.com>
2022-03-09 01:47:18 +0600

    update banner

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navigation from '../../../shared/Navigation/Navigation';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
-            <h1>This is home</h1>
+            <Navigation />
+            <Banner />
         </div>
     );
 };
